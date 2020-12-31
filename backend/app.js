b3069afe03f03982b2f34321dbccf20749b31164
norbertkross <norbertaberor@gmail.com>
2020-12-31 14:50:18 +0000

    created the api for the chatrooms

--- backend/app.js
+++ backend/app.js
@@ -2,18 +2,33 @@ var express = require('express');
 var app = express()
 const http = require('http').createServer(app)
 
-// const loginRouter = require('./requests/auth/normal_user/login_user');
+const readMSGRouter = require("./messages/readDRmessage.js");
+const sendDRmessage = require('./messages/sendDRmessage.js');
+const chatroom = require('./messages/getSpecifiqChatroom');
+const allDoctors = require('./messages/getAllDoctors');
+const sendFeedback = require('./feedback/insertFeedback.js');
+const allPrescriptions = require('./feedback/all_prescription.js');
+const addPrescriptions = require('./feedback/addPrescriptions.js');
+
 
 
 
 app.get('/', function(req, res){
-    res.send("<center><h2>WELCOME TO <h1>TEKCARE</h1></h2></center>");
+    res.send("<center> <h2>WELCOME TO <h1>TEKCARE</h1> </h2></center>");
   });
 
 
   // ########### use Custom routes########### //
 
         //app.use(loginRouter) // loginRouter was imported in the comment aboove
+        app.use(readMSGRouter) 
+        app.use(sendDRmessage) 
+        app.use(chatroom) 
+        app.use(allDoctors) 
+        app.use(sendFeedback) 
+        app.use(allPrescriptions) 
+        app.use(addPrescriptions) 
+
 
 
 
