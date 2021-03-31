3a87588a12f8f2481208c43414e1657ca00c187b
ekyeremeh7 <ekyeremeh7@gmail.com>
2021-03-31 16:29:29 +0000

    resized close button

--- mobile/lib/components/close_err_message_button.dart
+++ mobile/lib/components/close_err_message_button.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 
-
 class CloseErrorMessageButton extends StatelessWidget {
   CloseErrorMessageButton();
   // final Function onPressed;
@@ -8,31 +7,34 @@ class CloseErrorMessageButton extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: () {
-        Navigator.pop(context);
-      },
-      child: Wrap(children: [
-        Container(
-          width: MediaQuery.of(context).size.width*0.4,
-          child: Material(
-          color: Theme.of(context).primaryColor,
-          borderRadius: BorderRadius.circular(10.0),
-          elevation: 5.0,
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 10,vertical: 15),
-            child: Center(
-              child: Text(
-                'Close',
-                style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 15.0,
-                    fontWeight: FontWeight.w500),
+        onTap: () {
+          Navigator.pop(context);
+        },
+        child: Wrap(
+          children: [
+            Container(
+              padding: EdgeInsets.only(bottom: 15),
+              width: MediaQuery.of(context).size.width * 0.3,
+              child: Material(
+                color: Theme.of(context).primaryColor,
+                borderRadius: BorderRadius.circular(10.0),
+                elevation: 5.0,
+                child: Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 10, vertical: 15),
+                  child: Center(
+                    child: Text(
+                      'Close',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 15.0,
+                          fontWeight: FontWeight.w500),
+                    ),
+                  ),
+                ),
               ),
             ),
-          ),
-      ),
-        ),
-      ],)
-    );
+          ],
+        ));
   }
 }
